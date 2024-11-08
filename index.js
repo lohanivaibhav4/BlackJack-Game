3b3dc09a0342401e863152a3754814092dfa9188
Vaibhav Lohani <lohanivaibhav4@gmail.com>
2024-11-08 16:16:31 +0530

    Added Player Features

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-let firstCard = 15;
-let secondCard = 6;
-let newCard = 5;
-let sum = firstCard + secondCard ;
+let cards = [];
+let sum = 0 ;
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false;
 let message = ""
 let messEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum")
 let cardEl = document.getElementById("cards")
 
+let player = {
+    name: "Player",
+    chips: 100
+}
+
+let playerEl = getElementById("player-el")
+playerEl.textContent = player.name + ": Rs." +player.chips
+
 
 function startGame(){
+    isAlive = true;
+    hasBlackJack = false;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame()
 }
 
 function renderGame(){
 
-    cardEl.textContent ="Cards :- " + firstCard +" " + secondCard
+    cardEl.textContent = "Cards: "
+
+    for(let i =0; i<cards.length;i++)
+    {
+        cardEl.textContent += " " + cards[i]
+    }
     sumEl.textContent = "Sum :- " + sum
 
     if (sum < 21) {
@@ -35,12 +52,25 @@ function renderGame(){
     messEl.textContent = message;
 }
 function newCards(){
-    sum += newCard;
-    sumEl.textContent = "Sum :- " + sum
-    cardEl.textContent = "Cards :- " + firstCard +" " + secondCard + " " + newCard
-    renderGame();
+    if(isAlive === true && hasBlackJack ===false)
+    {
+        let newCard = getRandomCard();
+        sum += newCard;
+        cards.push(newCard);
+        renderGame();
+    }
+    else
+    {
+        messEl.textContent = "Invalid Action !";
+    }
 }
 function resetGame(){
     
+}
+function getRandomCard(){
+    let num = Math.floor(Math.random() * 13) + 1 ;
+    if(num === 1) return 11;
+    else if (num >=11 ) return 10;
+    else return num;
 }
 
